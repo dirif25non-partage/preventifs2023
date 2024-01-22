b0471dcf5ae3370b28396316b82c65017cd8c27a
dirif25non-partage <156591976+dirif25non-partage@users.noreply.github.com>
2024-01-22 09:55:59 +0100

    Add files via upload

---
+++ conf.py
@@ -0,0 +1,26 @@
+project = 'doc Sucombe'
+copyright = '2043, on'
+author = 'on'
+release = ''
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+html_theme = 'furo'
+html_favicon = 'https://storage.googleapis.com/idf-congestion/favicon.ico'
+html_static_path = ['_static']
+html_show_copyright = False
+html_show_sphinx = False
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "light_logo": "logoSucombe.jpg",
+    "dark_logo":  "logoSucombe.jpg",
+}
